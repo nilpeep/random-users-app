9f49c087a7fe9cee7c2fec7a0eb0e67d6cfec225
nilpeep <niluferk038@gmail.com>
2024-01-07 21:13:32 +0300

    bugs fixed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-import Footer from "./components/footer/Footer";
+
 import Card from "./components/Card.jsx/Card";
 
 function App() {
   return (
     <>
       <Card />
-      <Footer />
+     
     </>
   );
 }
