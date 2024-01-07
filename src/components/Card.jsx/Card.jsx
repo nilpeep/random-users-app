0f50c034271cade9fbd37036d9685330f26f6a52
nilpeep <niluferk038@gmail.com>
2024-01-07 21:18:33 +0300

    bugs fixed

--- src/components/Card.jsx/Card.jsx
+++ src/components/Card.jsx/Card.jsx
@@ -1,17 +1,12 @@
 import mailSvg from "../../assets/mail.svg";
 import manSvg from "../../assets/man.svg";
 import womanSvg from "../../assets/woman.svg";
-import manAgeSvg from "../../assets/growing-up-man.svg";
 import womanAgeSvg from "../../assets/growing-up-woman.svg";
 import mapSvg from "../../assets/map.svg";
 import phoneSvg from "../../assets/phone.svg";
 import padlockSvg from "../../assets/padlock.svg";
-import cwSvg from "../../assets/cw.svg";
-import bird from "../../assets/bird.jpg";
 import axios from "axios";
 import { useState, useEffect } from "react";
-
-const defaultImage = "https://randomuser.me/api/portraits/men/75.jpg";
 
 export default function Card() {
   const [activeUser, setActiveUser] = useState(null);
@@ -77,7 +72,7 @@ export default function Card() {
               className="icon"
               data-label="name"
             >
-              <img src={womanSvg} alt="user" id="iconImg" />
+              <img src={activeUser.gender === 'female' ? womanSvg : manSvg} alt="user" id="iconImg" />
             </button>
             <button
               onMouseOver={() => handleDisplay(activeUser.email, "email")}
