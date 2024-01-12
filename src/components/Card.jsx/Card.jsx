9e285b7e28318172d833e8c2a99031a25bd65c68
nilpeep <niluferk038@gmail.com>
2024-01-12 15:14:01 +0300

    deleteUser function

--- src/components/Card.jsx/Card.jsx
+++ src/components/Card.jsx/Card.jsx
@@ -6,6 +6,7 @@ import mapSvg from "../../assets/map.svg";
 import phoneSvg from "../../assets/phone.svg";
 import padlockSvg from "../../assets/padlock.svg";
 import axios from "axios";
+import DeleteIcon from "../../assets/delete.svg"
 import { useState, useEffect } from "react";
 import { toastErrorNotify,toastSuccessNotify } from "../../helper/ToastNotify";
 import { Spinner } from "react-bootstrap";
@@ -16,7 +17,11 @@ export default function Card() {
 
 
   const [activeUser, setActiveUser] = useState([]);
-  const [savedUsers, setSavedUsers] = useState([]);
+  const [savedUsers, setSavedUsers] = useState((() => {
+    // Uygulama yüklendiğinde localStorage'dan veriyi yükle
+    const localData = localStorage.getItem('users');
+    return localData ? JSON.parse(localData) : [];
+  }));
 
   const [activeInfo, setActiveInfo] = useState('');
   const [title, setTitle] = useState("name");
@@ -39,6 +44,8 @@ export default function Card() {
     ;
   };
 
+  
+
   const getRandomUser = async () => {
     try {
       const url = "https://randomuser.me/api/";
@@ -50,17 +57,17 @@ export default function Card() {
     }
   };
   
-  console.log(activeInfo)
 
   useEffect(() => {
     getRandomUser();
     
   }, []);
   
-  // useEffect(() =>{
-  //   setActiveInfo(`${activeUser?.name?.first} ${activeUser?.name?.last}`);
-
-  // }, [activeUser])
+  useEffect(() => {
+    // savedUsers state'i her güncellendiğinde localStorage'a kaydet
+    localStorage.setItem('users', JSON.stringify(savedUsers));
+    console.log(localStorage);
+  }, [savedUsers]);
 
   useEffect(() => {
     if (activeUser) {
@@ -73,6 +80,16 @@ export default function Card() {
     setActiveInfo(value);
     setTitle(title);
   };
+
+  const deleteUser = (selectedIndex) => {
+    // Filter methodu ile silinecek kullanıcı haricindeki kullanıcıları yeni bir diziye al
+    const updatedUsers = savedUsers.filter((user,index) => index !== selectedIndex);
+    
+    // Güncellenmiş kullanıcı listesini state'e ve localStorage'a kaydet
+    setSavedUsers(updatedUsers);
+    localStorage.setItem('users', JSON.stringify(updatedUsers));
+  };
+  console.log(savedUsers)
 
   return (
     <main>
@@ -172,6 +189,7 @@ export default function Card() {
           <th>Email</th>
           <th>Tel</th>
           <th>Age</th>
+          <th>delete</th>
         </tr>
       </thead>
       <tbody>
@@ -187,6 +205,10 @@ export default function Card() {
                     <td>{user.email}</td>
                     <td>{user.phone}</td>
                     <td>{user.dob.age}</td>
+                    <td onClick={() => deleteUser(i)} style={{cursor:'pointer'}} className="deleteItem">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="deleteSvg" height="16" width="14" viewBox="0 0 448 512"><path  d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>
+                     
+                    </td>
                   </tr>
                 );
               })}
